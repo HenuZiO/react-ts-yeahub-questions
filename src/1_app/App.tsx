0752e097e83b07dc0ec1c516cac1fc3c5d8036d0
Pavel Henuz <mshenuz@gmail.com>
2025-12-19 12:32:24 +0300

    Верстка сайдбара. Финальный вариант ПК-версии без логики. Создан универсальный компонент кнопок и SVG иконок. Добавление вспомогательной библиотеки для склейки стилей.

--- src/1_app/App.tsx
+++ src/1_app/App.tsx
@@ -1,114 +1,105 @@
+import { SendSquareIcon } from '../6_shared/ui/icon/variants/SendSquareIcon'
+import { HomeIcon } from '../6_shared/ui/icon/variants/HomeIcon'
+import { UserRoundedIcon } from '../6_shared/ui/icon/variants/UserRoundedIcon'
+import { BookIcon } from '../6_shared/ui/icon/variants/BookIcon'
+import { StudentIcon } from '../6_shared/ui/icon/variants/StudentIcon'
+import { BroadcastIcon } from '../6_shared/ui/icon/variants/BroadcastIcon'
+import { ChatCircleText } from '../6_shared/ui/icon/variants/ChatCircleText'
+import { LogoutIcon } from '../6_shared/ui/icon/variants/LogoutIcon'
+import { ChevroneDownIcon } from '../6_shared/ui/icon/variants/ChevroneDownIcon'
+import { ChevroneUpIcon } from '../6_shared/ui/icon/variants/ChevroneUpIcon'
+import { YeaLogo } from '../6_shared/ui/logo/YeaLogo'
+
+import styles from './styles.module.css'
+import { Button } from '../6_shared/ui/button/Button'
+
 function App() {
     
     return (
-        <>
-            <aside>
-                <header>
-                    <div>
-                        <img src='' alt='' width='33' height='33' loading='lazy' />
-                        <span>YeaHub</span>
-                    </div>
-                    <button type='button' aria-label='Свернуть меню'>
-                        {'<'}
+        <div className={styles.page}>
+            <aside className={styles.sidebar}>
+                <header className={styles.sidebar__header}>
+                    <YeaLogo />
+
+                    <button
+                        className={styles.header__button}
+                        type='button'
+                        aria-label='Свернуть меню'
+                    >
+                        <SendSquareIcon size={20} />
                     </button>
                 </header>
 
-                <nav aria-label='Основная навигация'>
-                    <ul>
+                <nav className={styles.sidebar__nav} aria-label='Основная навигация'>
+                    <ul className={styles.nav__list}>
                         <li>
-                            <a href='/'>
-                                <span aria-hidden='true'>[]</span>
+                            <a className={styles.item__link} href='/'>
+                                <HomeIcon />
                                 <span>Главная</span>
                             </a>
                         </li>
 
                         <li>
-                            <a href='/'>
-                                <span aria-hidden='true'>[]</span>
+                            <a className={styles.item__link} href='/'>
+                                <UserRoundedIcon />
                                 <span>Мой профиль</span>
                             </a>
                         </li>
-
-                        <li>
+                        
+                        <li className={styles.accordion__expanded}>
                             <button
+                                className={styles.item__button}
                                 type='button'
                                 aria-expanded='true'
                                 aria-controls='training-submenu'
                             >
-                                <span aria-hidden='true'>[]</span>
+                                <BookIcon />
                                 <span>Обучение</span>
+                                <ChevroneUpIcon className={styles.icon__accordion} />
                             </button>
 
-                            <ul id='training-submenu'>
+                            <ul className={styles.submenu} id='training-submenu'>
                                 <li>
-                                    <a href='/'>
-                                        <span aria-hidden='true'>[]</span>
+                                    <a className={styles.item__link} href='/'>
+                                        <StudentIcon />
                                         <span>Собеседование</span>
                                     </a>
                                 </li>
 
                                 <li>
-                                    <a href='/'>
-                                        <span aria-hidden='true'>[]</span>
+                                    <a className={styles.item__link} href='/'>
+                                        <BroadcastIcon />
                                         <span>Roadmap</span>
                                     </a>
                                 </li>
                             </ul>
                         </li>
-
+                        
                         <li>
                             <button
+                                className={styles.item__button}
                                 type='button'
-                                aria-expanded='true'
+                                aria-expanded='false'
                                 aria-controls='blog-submenu'
                             >
-                                <span aria-hidden='true'>[]</span>
+                                <BookIcon />
                                 <span>Блог</span>
+                                <ChevroneDownIcon className={styles.icon__accordion} />
                             </button>
-
-                            <ul id='blog-submenu'>
-                                <li>
-                                    <a href='/'>
-                                        <span aria-hidden='true'>[]</span>
-                                        <span>Все статьи</span>
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href='/'>
-                                        <span aria-hidden='true'>[]</span>
-                                        <span>Личный блог</span>
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href='/blog/new'>
-                                        <span aria-hidden='true'>[]</span>
-                                        <span>Написать статью</span>
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href='/blog/saved'>
-                                        <span aria-hidden='true'>[]</span>
-                                        <span>Сохранённые</span>
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
                     </ul>
                 </nav>
 
-                <footer>
-                    <a href='/'>
-                        <span aria-hidden='true'>[]</span>
+                <footer className={styles.sidebar__footer}>
+                    <Button variant='primary' as='a' href='/' aria-label='Поддержка в Telegram'>
+                        <ChatCircleText />
                         <span>Поддержка</span>
-                    </a>
+                    </Button>
 
-                    <button type='button'>
-                        <span aria-hidden='true'>[]</span>
+                    <Button variant='outline' aria-label='Выйти из аккаунта'>
+                        <LogoutIcon />
                         <span>Выход</span>
-                    </button>
+                    </Button>
                 </footer>
             </aside>
 
@@ -121,132 +112,131 @@ function App() {
                     <img src='' alt='' width='40' height='40' loading='lazy' />
                 </div>
             </header>
-
-            <main>
-                <nav>
-                    <ol>
-                        <li>
-                            <a href='/'>
-                                Обучение
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href='/'>
-                                Список вопросов
-                            </a>
-                        </li>
-                    </ol>
-                </nav>
-
-                <div>
-                    <section>
-                        <h2>Вопросы React, JavaScript</h2>
-
-                        <article>
-                            <header>
-                                <img
-                                    src=''
-                                    alt=''
-                                    width='8'
-                                    height='8'
-                                    loading='lazy'
-                                    aria-hidden='true'
-                                />
-                                <h3>Что такое Virtual DOM, и как он работает?</h3>
-                                <button type='button' aria-label='Развернуть ответ'>
-                                    <img src='' alt='' width='24' height='24' loading='lazy' />
-                                </button>
-                            </header>
-
-                            <div>
-                                <dl>
-                                    <div>
-                                        <dt>Рейтинг</dt>
-                                        <dd>4</dd>
-                                    </div>
-                                    <div>
-                                        <dt>Сложность</dt>
-                                        <dd>10</dd>
-                                    </div>
-                                </dl>
-                                
-                                <button  type='button' aria-label='Вопрос - действия'>
-                                    Настройки
-                                </button>
-                            </div>
-
-                            <div>
-                                <p>
-                                    Virtual DOM — это абстракция реального DOM, которая используется React для улучшения производительности.
-                                    Вместо того чтобы обновлять реальный DOM напрямую при каждом изменении состояния, React сначала обновляет виртуальный DOM, а затем сравнивает его с предыдущей версией, чтобы минимизировать количество операций с реальным DOM.
-                                    Этот процесс называется дифференциацией (reconciliation).
-                                </p>
-                                <pre>
-                                    <code>
-                                        [тут пример кода]
-                                    </code>
-                                </pre>
-                                
-                            </div>
-                        </article>
-
-                        <nav aria-label='Пагинация'>
-                            <button type='button' aria-label='Предыдущая страница'>
-                                {'<-'}
-                            </button>
-
-                            <ul>
-                                <li>
-                                    <button type='button'>
-                                        1
-                                    </button>
-                                </li>
-                                <li>
-                                    <button type='button'>
-                                        2
-                                    </button>
-                                </li>
-                                <li>
-                                    <button type='button'>
-                                        3
-                                    </button>
-                                </li>
-                                <li>
-                                    <button type='button'>
-                                        4
-                                    </button>
-                                </li>
-
-                                <li>
-                                    <span>...</span>
-                                </li>
-
-                                <li>
-                                    <button type='button'>
-                                        17
-                                    </button>
-                                </li>
-
-                                <li>
-                                    <button type='button'>
-                                        18
-                                    </button>
-                                </li>
-                            </ul>
-
-                            <button type='button' aria-label='Следующая страница'>
-                                {'->'}
-                            </button>
-                        </nav>
-                    </section>
-                    
-                    <section>
-                        Тут будут фильтра вопросов
-                    </section>
-                </div>
-            </main>
-        </>
+            {/*<main>*/}
+            {/*    <nav>*/}
+            {/*        <ol>*/}
+            {/*            <li className={styles.list__item}>*/}
+            {/*                <a href='/'>*/}
+            {/*                    Обучение*/}
+            {/*                </a>*/}
+            {/*            </li>*/}
+            
+            {/*            <li className={styles.list__item}>*/}
+            {/*                <a href='/'>*/}
+            {/*                    Список вопросов*/}
+            {/*                </a>*/}
+            {/*            </li>*/}
+            {/*        </ol>*/}
+            {/*    </nav>*/}
+            
+            {/*    <div>*/}
+            {/*        <section>*/}
+            {/*            <h2>Вопросы React, JavaScript</h2>*/}
+            
+            {/*            <article>*/}
+            {/*                <header>*/}
+            {/*                    <img*/}
+            {/*                        src=''*/}
+            {/*                        alt=''*/}
+            {/*                        width='8'*/}
+            {/*                        height='8'*/}
+            {/*                        loading='lazy'*/}
+            {/*                        aria-hidden='true'*/}
+            {/*                    />*/}
+            {/*                    <h3>Что такое Virtual DOM, и как он работает?</h3>*/}
+            {/*                    <button type='button' aria-label='Развернуть ответ'>*/}
+            {/*                        <img src='' alt='' width='24' height='24' loading='lazy' />*/}
+            {/*                    </button>*/}
+            {/*                </header>*/}
+            
+            {/*                <div>*/}
+            {/*                    <dl>*/}
+            {/*                        <div>*/}
+            {/*                            <dt>Рейтинг</dt>*/}
+            {/*                            <dd>4</dd>*/}
+            {/*                        </div>*/}
+            {/*                        <div>*/}
+            {/*                            <dt>Сложность</dt>*/}
+            {/*                            <dd>10</dd>*/}
+            {/*                        </div>*/}
+            {/*                    </dl>*/}
+            
+            {/*                    <button type='button' aria-label='Вопрос - действия'>*/}
+            {/*                        Настройки*/}
+            {/*                    </button>*/}
+            {/*                </div>*/}
+            
+            {/*                <div>*/}
+            {/*                    <p>*/}
+            {/*                        Virtual DOM — это абстракция реального DOM, которая используется React для улучшения производительности.*/}
+            {/*                        Вместо того чтобы обновлять реальный DOM напрямую при каждом изменении состояния, React сначала обновляет виртуальный DOM, а затем сравнивает его с предыдущей версией, чтобы минимизировать количество операций с реальным DOM.*/}
+            {/*                        Этот процесс называется дифференциацией (reconciliation).*/}
+            {/*                    </p>*/}
+            {/*                    <pre>*/}
+            {/*                        <code>*/}
+            {/*                            [тут пример кода]*/}
+            {/*                        </code>*/}
+            {/*                    </pre>*/}
+            
+            {/*                </div>*/}
+            {/*            </article>*/}
+            
+            {/*            <nav aria-label='Пагинация'>*/}
+            {/*                <button type='button' aria-label='Предыдущая страница'>*/}
+            {/*                    {'<-'}*/}
+            {/*                </button>*/}
+            
+            {/*                <ul>*/}
+            {/*                    <li className={styles.list__item}>*/}
+            {/*                        <button type='button'>*/}
+            {/*                            1*/}
+            {/*                        </button>*/}
+            {/*                    </li>*/}
+            {/*                    <li className={styles.list__item}>*/}
+            {/*                        <button type='button'>*/}
+            {/*                            2*/}
+            {/*                        </button>*/}
+            {/*                    </li>*/}
+            {/*                    <li className={styles.list__item}>*/}
+            {/*                        <button type='button'>*/}
+            {/*                            3*/}
+            {/*                        </button>*/}
+            {/*                    </li>*/}
+            {/*                    <li className={styles.list__item}>*/}
+            {/*                        <button type='button'>*/}
+            {/*                            4*/}
+            {/*                        </button>*/}
+            {/*                    </li>*/}
+            
+            {/*                    <li className={styles.list__item}>*/}
+            {/*                        <span>...</span>*/}
+            {/*                    </li>*/}
+            
+            {/*                    <li className={styles.list__item}>*/}
+            {/*                        <button type='button'>*/}
+            {/*                            17*/}
+            {/*                        </button>*/}
+            {/*                    </li>*/}
+            
+            {/*                    <li className={styles.list__item}>*/}
+            {/*                        <button type='button'>*/}
+            {/*                            18*/}
+            {/*                        </button>*/}
+            {/*                    </li>*/}
+            {/*                </ul>*/}
+            
+            {/*                <button type='button' aria-label='Следующая страница'>*/}
+            {/*                    {'->'}*/}
+            {/*                </button>*/}
+            {/*            </nav>*/}
+            {/*        </section>*/}
+            
+            {/*        <section>*/}
+            {/*            Тут будут фильтра вопросов*/}
+            {/*        </section>*/}
+            {/*    </div>*/}
+            {/*</main>*/}
+        </div>
     )
 }
 
---
+++ src/6_shared/ui/icon/variants/ChevroneDownIcon.tsx
@@ -0,0 +1,16 @@
+import { BaseIcon } from '../BaseIcon'
+import type { IconProps } from '../BaseIcon'
+
+export const ChevroneDownIcon = (props: IconProps) => {
+    return (
+        <BaseIcon viewBox='0 0 24 24' {...props}>
+            <path
+                d='M6 9L12 15L18 9'
+                stroke='currentColor'
+                strokeWidth='1.66667'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+        </BaseIcon>
+    )
+}
