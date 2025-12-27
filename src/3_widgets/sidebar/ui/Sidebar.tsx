fe9de169cf1acab9f0e343f57c06b5fbc00c26f8
Pavel Henuz <mshenuz@gmail.com>
2025-12-27 20:23:53 +0300

    Доработка Sidebar - добавлен функционал сворачивания/разворачивания подменю при нажатии на пункт меню. Обновление/добавление иконок для сайдбара.

--- src/3_widgets/sidebar/ui/Sidebar.tsx
+++ src/3_widgets/sidebar/ui/Sidebar.tsx
@@ -1,22 +1,25 @@
+import { useState } from 'react'
 import { Button } from '@/6_shared/ui/button'
 import { YeaLogo } from '@/6_shared/ui/logo'
 import { cn } from '@/6_shared/lib/utils/classnames'
+import { SIDEBAR_ITEMS } from '../config/menuConfig'
 
-import {
-    BookIcon,
-    BroadcastIcon,
-    ChatCircleText,
-    ChevroneUpIcon,
-    HomeIcon,
-    LogoutIcon,
-    SendSquareIcon,
-    StudentIcon,
-    UserRoundedIcon
-} from '@/6_shared/ui/icon'
+import { ChatCircleText, ChevroneDownIcon, LogoutIcon, SendSquareIcon } from '@/6_shared/ui/icon'
 
 import styles from './Sidebar.module.css'
 
 export const Sidebar = () => {
+    const [expanded, setExpanded] = useState<Record<string, boolean>>({
+        training: false
+    })
+    
+    const handleGroupToggle = (id: string) => {
+        setExpanded(prev => ({
+            ...prev,
+            [id]: !prev[id]
+        }))
+    }
+    
     return (
         <aside className={styles.sidebar}>
             <header className={styles.sidebar__header}>
@@ -33,64 +36,82 @@ export const Sidebar = () => {
 
             <nav className={styles.sidebar__nav} aria-label='Основная навигация'>
                 <ul className={styles.nav__list}>
-                    <li>
-                        <a className={styles.nav__item} href='/'>
-                                <HomeIcon />
-                                <span>Главная</span>
-                            </a>
-                    </li>
-
-                    <li>
-                        <a className={styles.nav__item} href='/'>
-                                <UserRoundedIcon />
-                                <span>Мой профиль</span>
-                        </a>
-                    </li>
-
-                    <li className={styles.nav__item_expanded}>
-                        <button
-                            className={styles.nav__item}
-                            type='button'
-                            aria-expanded='true'
-                            aria-controls='training-submenu'
-                        >
-                            <BookIcon />
-                            <span>Обучение</span>
-                            <ChevroneUpIcon className={styles.nav__icon_chevron} />
-                        </button>
-
-                        <ul className={styles.nav__submenu} id='training-submenu'>
-                            <li>
-                                <a
-                                    className={cn(styles.nav__item, styles.nav__item_submenu)}
-                                    href='/'
+                    {SIDEBAR_ITEMS.map(item => {
+                        if (item.type === 'link') {
+                            const { id, href, label, Icon } = item
+                            
+                            return (
+                                <li key={id}>
+                                    <a className={styles.nav__item} href={href}>
+                                        <Icon />
+                                        <span>{label}</span>
+                                    </a>
+                                </li>
+                            )
+                        }
+                        
+                        if (item.type === 'group') {
+                            const isOpen = expanded[item.id]
+                            const { id, label, Icon, children } = item
+                            
+                            return (
+                                <li
+                                    key={id}
+                                    className={styles.nav__group}
+                                    data-expanded={isOpen}
                                 >
-                                        <StudentIcon />
-                                        <span>Собеседование</span>
-                                </a>
-                            </li>
+                                    <button
+                                        className={styles.nav__item}
+                                        type='button'
+                                        aria-expanded={isOpen}
+                                        aria-controls={`${id}-submenu`}
+                                        onClick={() => handleGroupToggle(id)}
+                                    >
+                                        <Icon />
+                                        <span>{label}</span>
 
-                            <li>
-                                <a
-                                    className={cn(styles.nav__item, styles.nav__item_submenu)}
-                                    href='/'
-                                >
-                                    <BroadcastIcon />
-                                    <span>Roadmap</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+                                        <span className={styles.nav__chevron}>
+                                            <ChevroneDownIcon />
+                                        </span>
+                                    </button>
+
+                                    <div
+                                        className={styles.nav__submenu}
+                                        id={`${id}-submenu`}
+                                    >
+                                        <div className={styles.nav__submenu_inner}>
+                                            {children.map(child => {
+                                                const { id, href, label, Icon } = child
+                                                
+                                                return (
+                                                    <a
+                                                        className={cn(styles.nav__item, styles.nav__item_submenu)}
+                                                        href={href}
+                                                        key={id}
+                                                    >
+                                                        <Icon />
+                                                        <span>{label}</span>
+                                                    </a>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                </li>
+                            )
+                        }
+                        
+                        return null
+                    })}
                 </ul>
             </nav>
 
             <footer className={styles.sidebar__footer}>
-                <Button variant='primary' as='a' href='/' aria-label='Поддержка в Telegram'>
+                <Button variant='primary' as='a' href='/'>
                     <ChatCircleText />
                     <span>Поддержка</span>
                 </Button>
 
-                <Button variant='outline' aria-label='Выйти из аккаунта'>
+                <Button variant='outline'>
                     <LogoutIcon />
                     <span>Выход</span>
                 </Button>
