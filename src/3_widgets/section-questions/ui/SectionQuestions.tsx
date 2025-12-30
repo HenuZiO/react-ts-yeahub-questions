f91ece1940b8aa11b0dfa96e160f30f89c4bbfaa
Pavel Henuz <mshenuz@gmail.com>
2025-12-30 13:00:45 +0300

    Добавление нового стиля outline для Button. Создание компонента EmptyState для отображения при отстутствии результатов поиска.

--- src/3_widgets/section-questions/ui/SectionQuestions.tsx
+++ src/3_widgets/section-questions/ui/SectionQuestions.tsx
@@ -4,12 +4,13 @@ import { selectQuestionsQueryParams } from '@/4_features/questions-filters'
 import { QuestionItem, useGetQuestionsQuery } from '@/5_entities/question'
 import { SectionTitle } from '@/6_shared/ui/section-title'
 import { Section } from '@/6_shared/ui/section'
+import { EmptyState } from '@/6_shared/ui/empty-state/ui/EmptyState'
 
 type SectionProps = {
     className: string
 }
 
-export const SectionQuestions = ({className}: SectionProps) => {
+export const SectionQuestions = ({ className }: SectionProps) => {
     const queryParams = useAppSelector(selectQuestionsQueryParams)
     
     const { data, isLoading } = useGetQuestionsQuery(queryParams)
@@ -18,15 +19,19 @@ export const SectionQuestions = ({className}: SectionProps) => {
         return <div>Идет загрузка данных…</div>
     }
     
+    const questionsList = data?.questions ?? []
+    
     return (
         <Section className={className}>
             <SectionTitle>
                 Вопросы для собеседований
             </SectionTitle>
             
-            {data?.questions.map(question => (
-                <QuestionItem key={question.id} question={question}/>
-            ))}
+            {questionsList.length > 0
+                ? questionsList.map(question => (
+                    <QuestionItem key={question.id} question={question} />
+                ))
+                : <EmptyState />}
             
             <QuestionsPagination />
         </Section>
