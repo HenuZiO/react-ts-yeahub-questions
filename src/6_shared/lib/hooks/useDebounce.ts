0cfde0e922a8b03ccaba665dcf49567f0b1bc254
Pavel Henuz <mshenuz@gmail.com>
2026-01-02 16:47:03 +0300

    Добавление хука useDebounce и обновление компонента FilterByTitle для фильтрации вопросов по названию. Изменение в работе логики запросов к серверу, добавлена задержка отправки запроса после ввода символов в поле для поиска.

---
+++ src/6_shared/lib/hooks/useDebounce.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+
+export const useDebounce = <T>(value: T, delay = 300): T => {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value)
+    
+    useEffect(() => {
+        const timerId = setTimeout(() => setDebouncedValue(value), delay)
+        
+        return () => clearTimeout(timerId)
+    }, [value, delay])
+    
+    return debouncedValue
+}
